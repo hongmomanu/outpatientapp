69e5352fcabb11adfa80c68dcbc31a56d334ab49
hongmomanu <hongmomanu@163.com>
2015-08-14 11:26:26 +0800

    jack commit

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'app.directives', 'app.services', 'app.controllers' ])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,94 +16,5 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
-
-.controller('appCtrl', function($scope, $interval,$timeout) {
-      var socket=null;
-
-      console.log('appCtrl');
-      var websocketInit=function(){
-
-        var url=localStorage.serverurl;
-        var area=localStorage.area;
-        if(!url||url==""){
-          //Ext.Msg.alert('提示','服务地址为空');
-          return ;
-        }
-        if(!area||area==""){
-          //Ext.Msg.alert('提示','诊区为空');
-          return ;
-        }
-        //url=url?"ws://"+url.split("://")[1].split(":")[0]+":3001/":"ws://localhost:3001/";
-        url=url.replace(/(:\d+)/g,":3001");
-        url=url.replace("http","ws");
-        socket = new WebSocket(url);
-
-        socket.onmessage = function(event) {
-          var data=JSON.parse(event.data);
-
-        };
-        socket.onclose = function(event) {
-
-          $timeout(function(){
-            websocketInit()
-          },3000)
-
-        };
-
-        socket.onopen = function() {
-
-          socket.send(JSON.stringify({
-            type:"mainscreen",
-            content: localStorage.area
-          }));
-        };
-
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      /*$scope.data1 ={title:"诊室1",data:[
-        {name:"jack",value:"sss"},
-        {name:"jack",value:"sss"},
-        {name:"jack",value:"sss"}
-      ]};
-      $scope.data2 ={title:"诊室2",data:[
-        {name:"jack",value:"sss"},
-        {name:"jack",value:"sss"},
-        {name:"jack",value:"sss"}
-      ]};
-      $scope.data3 ={title:"诊室3",data:[
-        {name:"jack",value:"sss"},
-        {name:"jack",value:"sss"},
-        {name:"jack",value:"sss"}
-      ]};
-
-*/
-      /*$timeout( function() {
-
-        //$scope.data1.data.push({name:"jackww",value:"sss"})
-        $scope.data1={title:"诊室33",data:[
-          {name:"jack",value:"sss"},
-          {name:"jack",value:"sss"},
-          {name:"jack",value:"sss"}
-        ]};
-
-      }, 5000);*/
-
-
 });
+
---
+++ www/js/controllers/initWebSocket.js
@@ -0,0 +1,115 @@
+/**
+ * Created by jack on 15-8-14.
+ */
+angular.module('app.controllers')
+    .controller('initWebSocket', function($scope, $interval,$timeout, $ionicModal) {
+        /*$scope.socket=null;
+        $scope.configdata=localStorage.configdata?JSON.parse(localStorage.configdata):{};
+
+        console.log('initWebSocket');
+
+        $ionicModal.fromTemplateUrl('templates/config.html', {
+            scope: $scope
+        }).then(function(modal) {
+            $scope.configmodal = modal;
+        });
+
+        //make config
+        $scope.makeConfig=function(configdata){
+            localStorage.configdata=JSON.stringify(configdata);
+            $scope.configmodal.hide();
+            window.location.reload();
+
+        };*/
+
+
+        $scope.websocketInit=function(){
+
+            var url=$scope.configdata.serverurl;
+            var areanum=$scope.configdata.areanum;
+            if(!url||url==""){
+                //Ext.Msg.alert('提示','服务地址为空');
+                $scope.configmodal.show();
+                return ;
+            }
+            if(!areanum||areanum==""){
+                //Ext.Msg.alert('提示','诊区为空');
+                $scope.configmodal.show()
+                return ;
+            }
+            //url=url?"ws://"+url.split("://")[1].split(":")[0]+":3001/":"ws://localhost:3001/";
+            url=url.replace(/(:\d+)/g,":3001");
+            url=url.replace("http","ws");
+            $scope.socket = new WebSocket(url);
+
+            $scope.socket.onmessage = function(event) {
+                var data=JSON.parse(event.data);
+                console.log(data);
+
+            };
+            $scope.socket.onclose = function(event) {
+
+                $timeout(function(){
+                    $scope.websocketInit()
+                },3000)
+
+            };
+
+            $scope.socket.onopen = function() {
+
+                $scope.socket.send(JSON.stringify({
+                    type:"mainscreen",
+                    content: areanum
+                }));
+            };
+
+        };
+
+        //$scope.websocketInit();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*$scope.data1 ={title:"诊室1",data:[
+         {name:"jack",value:"sss"},
+         {name:"jack",value:"sss"},
+         {name:"jack",value:"sss"}
+         ]};
+         $scope.data2 ={title:"诊室2",data:[
+         {name:"jack",value:"sss"},
+         {name:"jack",value:"sss"},
+         {name:"jack",value:"sss"}
+         ]};
+         $scope.data3 ={title:"诊室3",data:[
+         {name:"jack",value:"sss"},
+         {name:"jack",value:"sss"},
+         {name:"jack",value:"sss"}
+         ]};
+
+         */
+        /*$timeout( function() {
+
+         //$scope.data1.data.push({name:"jackww",value:"sss"})
+         $scope.data1={title:"诊室33",data:[
+         {name:"jack",value:"sss"},
+         {name:"jack",value:"sss"},
+         {name:"jack",value:"sss"}
+         ]};
+
+         }, 5000);*/
+
+
+    });
