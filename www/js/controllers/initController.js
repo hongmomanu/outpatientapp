d046b2f31ff401f59eb29103a6c78685d3219866
hongmomanu <hongmomanu@163.com>
2015-08-18 15:34:17 +0800

    jack commit

--- www/js/controllers/initController.js
+++ www/js/controllers/initController.js
@@ -2,116 +2,117 @@
  * Created by jack on 15-8-14.
  */
 angular.module('app.controllers')
-    .controller('initController', function($scope, $interval,$timeout, $ionicModal,$rootScope,$ionicLoading) {
-        if(!localStorage.totaltimes)localStorage.totaltimes=1;
-        $scope.socket=null;
-        $scope.speaktimes=0;
-        $scope.playlist=[];
-        $scope.isplaying=false;
-        $scope.callingindex=0;
+    .controller('initController', function ($scope, $interval, $timeout, $ionicModal, $rootScope, $ionicLoading) {
+        if (!localStorage.totaltimes)localStorage.totaltimes = 1;
+        $scope.socket = null;
+        $scope.speaktimes = 0;
+        $scope.playlist = [];
+        $scope.isplaying = false;
+        $scope.callingindex = 0;
 
-        $scope.configdata=localStorage.configdata?JSON.parse(localStorage.configdata):{};
+        $scope.configdata = localStorage.configdata ? JSON.parse(localStorage.configdata) : {};
 
         console.log('initController');
 
         $ionicModal.fromTemplateUrl('templates/config.html', {
             scope: $scope
-        }).then(function(modal) {
+        }).then(function (modal) {
             $scope.configmodal = modal;
             $rootScope.$broadcast('initWebSocket', $scope);
 
         });
 
         //make config
-        $scope.makeConfig=function(configdata){
-            localStorage.configdata=JSON.stringify(configdata);
+        $scope.makeConfig = function (configdata) {
+            localStorage.configdata = JSON.stringify(configdata);
             $scope.configmodal.hide();
             window.location.reload();
 
         };
 
-        $scope.playvoice=function(text){
+        $scope.playvoice = function (text) {
             $scope.speaktimes++;
-            try{
+            try {
                 //navigator.speech.startSpeaking( text , {voice_name: 'xiaoyan',speed: localStorage.speed} );
-                navigator.speech.startSpeaking( text[0] , {voice_name: 'xiaoyan',speed: '30'} );
-                setTimeout(function(){
-                    navigator.speech.startSpeaking( text[1]+'.'+text[1] , {voice_name: 'xiaoyan',speed: '10'} );
+                navigator.speech.startSpeaking(text[0], {voice_name: 'xiaoyan', speed: '30'});
+                setTimeout(function () {
+                    navigator.speech.startSpeaking(text[1] + '.' + text[1], {voice_name: 'xiaoyan', speed: '10'});
 
-                },2000);
+                }, 2000);
 
 
-                setTimeout(function(){
-                    navigator.speech.startSpeaking( text[2] , {voice_name: 'xiaoyan',speed: '30'} );
-                },(function(str){
-                    if(!str||str=="")return 6000;
-                    else if(str.length==2)return 5400;
-                    else if(str.length==3)return 6400;
-                    else if(str.length==4)return 6800;
+                setTimeout(function () {
+                    navigator.speech.startSpeaking(text[2], {voice_name: 'xiaoyan', speed: '30'});
+                }, (function (str) {
+                    if (!str || str == "")return 6000;
+                    else if (str.length == 2)return 5400;
+                    else if (str.length == 3)return 6400;
+                    else if (str.length == 4)return 6800;
                     else return 6000;
                 }(text[1])));
 
 
-            }catch (e){}
-            finally{
-                setTimeout(function(){
-                    if($scope.speaktimes>=localStorage.totaltimes){
-                        $scope.speaktimes=0;
+            } catch (e) {
+            }
+            finally {
+                setTimeout(function () {
+                    if ($scope.speaktimes >= localStorage.totaltimes) {
+                        $scope.speaktimes = 0;
                         delete $scope.playlist[$scope.callingindex];
 
                         $scope.callingindex++;
                         $ionicLoading.hide();
                         $interval.cancel($scope.timer);
                         $scope.makevoiceanddisplay();
-                    }else{
+                    } else {
                         //tipvoice.removeEventListener('ended',voiceEnd,false);
 
                         $scope.playvoice(text);
                     }
-                },10000);
-            };
-        } ;
+                }, 10000);
+            }
+            ;
+        };
 
 
-        $scope.showcallmsg=function(item){
+        $scope.showcallmsg = function (item) {
             $ionicLoading.show({
-                template: '<div style="font-size: 60px;vertical-align: middle;text-align: center;">'+'<a style="font-weight: bold">'+item.hzxh+'</a>'
-                +'<a style="font-weight: bold">'+item.hzxm+'</a>'
-                +'<a style="font-weight: bold">'+item.zsmc+'</a>'
-                +'</div>',
+                template: '<div style="font-size: 80px;line-height: normal;text-align: left;">' + '<a style="font-weight: bold">序号:' + item.hzxh + '</a>'
+                + '<br><a style="font-weight: bold">姓名:' + item.hzxm + '</a>'
+                + '<br><a style="font-weight: bold">诊室:' + item.zsmc + '</a>'
+                + '</div>',
                 animation: 'fade-in',
-                maxWidth: 200,
-                showBackdrop: true
+                /*maxWidth: 200,*/
+                showBackdrop: false
 
             });
         };
-        $scope.makevoiceanddisplay=function(){
+        $scope.makevoiceanddisplay = function () {
 
-            if(($scope.playlist.length-1)>=$scope.callingindex){
+            if (($scope.playlist.length - 1) >= $scope.callingindex) {
 
 
-                var item=$scope.playlist[$scope.callingindex];
+                var item = $scope.playlist[$scope.callingindex];
                 //var text="请 "+item.showno+item.patname+" 到"+item.roomname+"机房门口等候检查";
-                var text=["请 "+item.hzxh,item.hzxm," 到"+item.zsmc+"诊室等候问诊"];
+                var text = ["请 " + item.hzxh, item.hzxm, " 到" + item.zsmc + "诊室等候问诊"];
                 console.log(text);
 
                 $scope.showcallmsg(item);
-                $scope.timer=$interval(function(){
+                $scope.timer = $interval(function () {
                     $ionicLoading.hide();
 
-                   $timeout(function(){
-                       $scope.showcallmsg(item);
+                    $timeout(function () {
+                        $scope.showcallmsg(item);
 
-                   },1000);
-                },3500)
+                    }, 1000);
+                }, 3500)
 
 
                 $scope.playvoice(text);
 
 
-
-            }else{
-                $scope.isplaying=false;
+            } else {
+                $scope.isplaying = false;
                 /*me.isplaying=false;
                  me.playlist=[];*/
                 /*navigator.speech.removeEventListener("SpeakCompleted",function(){});
@@ -119,22 +120,16 @@ angular.module('app.controllers')
             }
 
 
-
         };
 
-        $scope.makeSpeak=function(data){
-            $scope.playlist=$scope.playlist.concat(data);
-            if(!$scope.isplaying){
-                $scope.isplaying=true;
+        $scope.makeSpeak = function (data) {
+            $scope.playlist = $scope.playlist.concat(data);
+            if (!$scope.isplaying) {
+                $scope.isplaying = true;
                 $scope.makevoiceanddisplay();
             }
-          //console.log(data);
+            //console.log(data);
         };
-
-
-
-
-
 
 
     });
